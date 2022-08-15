543965b60d4858c899a21f2c0818dda6b3f35467
classico345 <gylang167@gmail.com>
2022-08-15 13:06:47 +0700

    menambahkan hapus_siswa

--- application/controllers/Data_Siswa.php
+++ application/controllers/Data_Siswa.php
@@ -42,6 +42,7 @@ class Data_Siswa extends CI_Controller
 		$agama.$nik.$jenis_kelamin.$nidn.$ijazah.$skhun.$foto.$kelas.$nilai_ipa.$nilai_ips.
 		$nilai_bahasa_inggris.$nilai_bahasa_indonesia.$id_status_verifikasi.$id_status_lulus.rand(1,9999));
 	}
+
 	public function edit_siswa()
 	{
 
@@ -71,8 +72,33 @@ class Data_Siswa extends CI_Controller
 
 	}
 
-
-
+	public function hapus_siswa()
+	{
+		
+		$id_user_detail = $this->input->post('id_user_detail');
+		$nama_lengkap = $this->input->post('nama_lengkap');
+		$alamat = $this->input->post('alamat');
+		$tempat_lahir = $this->input->post('tempat_lahir');
+		$tanggal_lahir = $this->input->post('tanggal_lahir');
+		$agama = $this->input->post('agama');
+		$nik = $this->input->post('nik');
+		$jenis_kelamin = $this->input->post('jenis_kelamin');
+		$nidn = $this->input->post('nidn');
+		$ijazah = $this->input->post('ijazah');
+		$skhun = $this->input->post('skhun');
+		$foto = $this->input->post('foto');
+		$kelas = $this->input->post('kelas');
+		$nilai_ipa = $this->input->post('nilai_ipa');
+		$nilai_ips = $this->input->post('nilai_ips');
+		$nilai_matematika = $this->input->post('nilai_matematika');
+		$nilai_bahasa_inggris = $this->input->post('nilai_bahasa_inggris');
+		$nilai_bahasa_indonesia = $this->input->post('nilai_bahasa_indonesia');
+		$id_status_verifikasi = $this->input->post('id_status_verifikasi');
+		$id_status_lulus = $this->input->post('id_status_lulus');
+		$foto_name = md5($id_user_detail.$nama_lengkap.$alamat.$tempat_lahir.$tanggal_lahir.
+		$agama.$nik.$jenis_kelamin.$nidn.$ijazah.$skhun.$foto.$kelas.$nilai_ipa.$nilai_ips.
+		$nilai_bahasa_inggris.$nilai_bahasa_indonesia.$id_status_verifikasi.$id_status_lulus.rand(1,9999));
+	}
 
 	public function view_admin_terverifikasi()
 	{
